8173f1a23b92a0b3ff539283067eedd4cf893c16
apetitflores <apetitflores@gmail.com>
2021-03-07 12:21:52 -0400

    Project start.

---
+++ socket.js
@@ -0,0 +1,12 @@
+const { Server } = require("socket.io");
+const io = new Server();
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+	socket.on('chat message', (msg) => {
+		console.log(msg);
+		io.emit('chat message', msg);
+	});
+});
+
+module.exports = io;
